3e2fa413c3f9785fa9f77b6e5e01b334fa501dac
hoangnh621 <84167289+hoangnh621@users.noreply.github.com>
2022-04-29 15:58:35 +0700

    done yup demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,78 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+import * as yup from 'yup'
+
+const schema = yup.object().shape({
+  username: yup.string().required().matches(/^\S*$/),
+  password: yup.string().required().matches(/^\S*$/),
+})
+
+const StyledForm = styled.form`
+  width: 30%;
+  margin: auto;
+  padding: 15px;
+  background-color: tomato;
+  border-radius: 5px;
+  div {
+    display: flex;
+    justify-content: center;
+    margin-top: 15px;
+    label {
+      min-width: 100px;
+    }
+    input {
+      flex: 1;
+    }
+  }
+`
+
 function App() {
-  return <div className="App"></div>
+  const [userName, setUserName] = useState('')
+  const [userPassword, setUserPassword] = useState('')
+
+  //handle prevent default
+  const handlePreventDefault = (e) => {
+    e.preventDefault()
+  }
+  const check = async () => {
+    await schema
+      .isValid({
+        username: userName,
+        password: userPassword,
+      })
+      .then((valid) => {
+        if (valid) {
+          alert('OK')
+        } else {
+          alert('Invalid username or password')
+        }
+      })
+  }
+  return (
+    <div className="App">
+      <StyledForm onSubmit={handlePreventDefault}>
+        <div>
+          <label htmlFor="">User Name</label>
+          <input
+            type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input
+            type="text"
+            value={userPassword}
+            onChange={(e) => setUserPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={check}>Submit</button>
+        </div>
+      </StyledForm>
+    </div>
+  )
 }
 
 export default App
